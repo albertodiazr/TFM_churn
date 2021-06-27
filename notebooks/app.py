6a2a8843e97bcfaed85756041101b7856f9e8999
Alberto Diaz <albertodiazr@hotmail.com>
2021-06-27 17:12:25 +0200

    Pequeños cambios
    Pequeños cambios en los notebooks y en app.py

--- notebooks/app.py
+++ notebooks/app.py
@@ -5,6 +5,7 @@ import seaborn as sns
 import numpy as np
 import itertools
 import datetime as dt
+import matplotlib.pyplot as plt
 from tools import dataoveriew, plot_roc_curve, plot_confusion_matrix
 from datetime import datetime
 from sklearn.model_selection import cross_val_score, StratifiedKFold
@@ -46,7 +47,7 @@ def feature_engineering_data(data):
 def show_countplot(data):
 
     st.subheader("Data Visualization")
-    feature_x = st.selectbox("Seleccionar variable para la X", ['Tipo Propiedad','Gender', 'Tipo Inmueble', 'Provincia', 'Situacion Laboral', 'Estado Civil', 
+    feature_x = st.selectbox("Seleccionar variable para la X", ['Tipo Inmueble', 'Tipo Propiedad','Gender', 'Provincia', 'Situacion Laboral', 'Estado Civil', 
                        'Pais', 'Rango_Edad', 'Income','Precio Total', 'Precio Contado', 'Pagos Anuales'])
 
     feature_seg = st.selectbox("Seleccionar variable para segmentar", ['Precio Contado','Gender','Tipo Propiedad','Tipo Inmueble', 'Provincia', 'Situacion Laboral', 'Estado Civil', 
@@ -111,39 +112,37 @@ def machine_learning_model(data, data_to_result):
     
     elif classifier == "Voting Classifier":
         filename = '../pkl/voting_clf_model.pkl'
-        xgb = pickle.load(open(filename, 'rb'))       
-        y_pred = xgb.predict(X)
-        result = xgb.predict_proba(X)[:,1].reshape(-1, 1)
-        accuracy = xgb.score(X, y)
+        voting = pickle.load(open(filename, 'rb'))       
+        y_pred = voting.predict(X)
+        result = voting.predict_proba(X)[:,1].reshape(-1, 1)
+        accuracy = voting.score(X, y)
     
     else:
         st.stop()
 
-    class_names = ['Activo','Baja']
-
-    st.subheader("Metrics")
-    st.write("Accuracy: ", accuracy.round(2))
-    st.write("Precision: ", precision_score(y, y_pred, labels=class_names).round(2))
-    st.write("Recall: ", recall_score(y, y_pred, labels=class_names).round(2))
-
-    st.subheader("Confusion Matrix")
 
     # Desactivar warning:
     st.set_option('deprecation.showPyplotGlobalUse', False)
 
-    cm = confusion_matrix(y, y_pred)
-    plot_confusion_matrix(cm, classes=class_names, title='Confusion matrix')
-    st.pyplot()
-  
-    proba_baja = pd.DataFrame(result, columns = ['Probabilidad de baja'])
+    proba_baja = pd.DataFrame(result, columns = ['Churn_Probability'])
     resultado = pd.concat([data_to_result, proba_baja], axis = 1)
+    st.write('')  
     
+    st.header("Results Resume:")
+    resume = pd.crosstab(resultado['Tipo Inmueble'], resultado['Precio Contado'], values=resultado.Churn_Probability, aggfunc='mean').round(4)*100
+    fig, ax = plt.subplots(figsize=(11, 9))
+    cmap = sns.diverging_palette(220, 10, as_cmap=True)
+    sns.heatmap(resume, ax=ax, cmap=cmap, annot=True, cbar=False, linewidths=.5)
+    st.write(fig)
+
+    
+    st.write('')  
     st.header("Customers with the highest likelihood of churning:")
     
     threshold = st.slider('Mostar clientes con probabilidad de baja superior a (%): ', 
                           min_value=0.0, max_value=100.0, value = 70.0, step = 0.5)
-    resultado_filtrado = resultado[(resultado['Probabilidad de baja'] >= threshold/100)]
-    resultado_ordenado = resultado_filtrado.sort_values('Probabilidad de baja',ascending=False)
+    resultado_filtrado = resultado[(resultado['Churn_Probability'] >= threshold/100)]
+    resultado_ordenado = resultado_filtrado.sort_values('Churn_Probability',ascending=False)
     
     st.write("Treshold seleccionado (%): ", threshold)
     st.write("Número de clientes mostrados: ", resultado_ordenado.shape[0])    
@@ -153,9 +152,18 @@ def machine_learning_model(data, data_to_result):
         resultado_ordenado.to_excel("../data/prediction_result.xlsx", index = False)
 
 # Main
+
+# Set page config
+apptitle = 'Customer Churn app'
+st.set_page_config(page_title=apptitle, page_icon=":cry:")
+
 header_pic = Image.open('../images/churn_icon.png')
-st.image(header_pic, use_column_width=True)
+
 st.title("Customer Churn Prediction app")
+
+st.sidebar.image(header_pic, use_column_width=True)
+st.sidebar.markdown("## Customer Churn Prediction app")
+st.sidebar.markdown('Instrucciones para ejecutar la app:')
      
 uploaded_file = st.file_uploader("Subir archivo .xlsx", type="xlsx")
       
@@ -182,7 +190,6 @@ feature_engineering_data(data)
 data_to_result = data.copy()
 
 show_countplot(data)
-
 machine_learning_model(data, data_to_result)
 
 
